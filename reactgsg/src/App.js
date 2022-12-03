5e683f481932aedd03a73525359a408757ac3ab0
Ansam1999 <ansamghoul99@gmail.com>
2022-12-03 13:35:19 +0200

    app

--- reactgsg/src/App.js
+++ reactgsg/src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import WeatherSection from "./components/WeatherSection";
+import LiveCameras from "./components/LiveCameras";
 
 function App() {
-  return {
-    /* <div className="App">
-      <WeatherSection/>
-    </div>*/
-  };
+  return (
+    <div className="App">
+      <WeatherSection />
+      <LiveCameras />
+    </div>
+  );
 }
 
 export default App;
